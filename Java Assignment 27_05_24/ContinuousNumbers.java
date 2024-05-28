9a4fda321cc7fe89990522cf679ca48541367ab7
Swasthik Kulal <97526892+Swasthik99@users.noreply.github.com>
2024-05-28 18:17:44 +0530

    Update ContinuousNumbers.java

--- Java Assignment 27_05_24/ContinuousNumbers.java
+++ Java Assignment 27_05_24/ContinuousNumbers.java
@@ -1,5 +1,4 @@
 public class ContinuousNumbers {
-    // Method to check if there are three continuous numbers in the array
     public static boolean ThreeContinuousNumbers(int[] a) {
         for (int i = 0; i < a.length - 2; i++) {
             if (a[i + 1] == a[i] + 1 && a[i + 2] == a[i] + 2) {
