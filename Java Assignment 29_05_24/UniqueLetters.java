997da27ed5b6de1a9fb6d943bf28656a51a77b38
Swasthik Kulal <97526892+Swasthik99@users.noreply.github.com>
2024-05-31 16:39:11 +0530

    Update UniqueLetters.java

--- Java Assignment 29_05_24/UniqueLetters.java
+++ Java Assignment 29_05_24/UniqueLetters.java
@@ -1,17 +1,22 @@
 public class UniqueLetters {
-    static boolean UniqueOrNot(String s){
+    static int UniqueOrNot(String s){
+        int count =0;
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (s.indexOf(ch) != s.lastIndexOf(ch)) {
-                return false;
+                count++;
             }
         }
-        return true;
+        return count/2;
         
     }
     
     public static void main(String[] args) {
-        String s = "abcabcbb";
-        System.out.println(UniqueOrNot(s));
+        String s = "abceabse";
+        if(UniqueOrNot(s) == 0){
+            System.out.println("Unique"); 
+        }else{
+            System.out.println("Not Unique "+UniqueOrNot(s)+" are duplicate");
+        }
     }
 }
